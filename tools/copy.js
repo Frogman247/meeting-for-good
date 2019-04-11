927cb8b5e588e22978ec0a4e6ace8016bfe96170
Sibasish Subhadarshee <deadpool0247ishit@gmail.com>
2019-04-11 06:06:47 +0530

    Update copy.js

--- tools/copy.js
+++ tools/copy.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-
+console.log("5")
 const shelljs = require('shelljs');
 const addCheckMark = require('./checkmark');
 const path = require('path');
@@ -12,4 +12,4 @@ const cpy = path.join(__dirname, '../node_modules/cpy-cli/cli.js');
 shelljs.exec(`${cpy} server/api/email/templates/ownerNotification/* build/api/email/templates/ownerNotification/`, addCheckMark.bind(null, callback));
 shelljs.exec(`${cpy} server/api/email/templates/inviteGuests/* build/api/email/templates/inviteGuests/`, addCheckMark.bind(null, callback));
 shelljs.exec(`${cpy} server/api/email/templates/editAvailability/* build/api/email/templates/editAvailability/`, addCheckMark.bind(null, callback));
-
+console.log("6")
